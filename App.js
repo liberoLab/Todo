650c8bfba7d607fbaa2f4ca40002802fbade872a
liberoLab <libero221.lab@gmail.com>
2021-04-04 23:42:39 +0900

    제목(할 일 목록) 만듬

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, } from 'react-native';
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header />
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +14,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+
+/* 최신 expo는 <StatusBar style="auto" /> 가 생겼다.
+그래서 아래 같이 작성하면 위와 같은 효과를 얻을 수 있다.
+
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
+    backgroundColor: '#fff',
+  },
+});
+*/
